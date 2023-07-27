2bc363a6003814d40e5c7edb095f9bb4fcf2ee9a
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-27 23:08:32 +0100

    fix message

--- _op_div.c
+++ _op_div.c
@@ -17,7 +17,7 @@ void _op_div(stack_t **stack, unsigned int l_num)
 	{
 		free_args(g_vars.args);
 		fclose(g_vars.file);
-		fprintf(stderr, "L%u: can't sub, stack too short\n", l_num);
+		fprintf(stderr, "L%u: can't div, stack too short\n", l_num);
 		exit(EXIT_FAILURE);
 	}
 	val1 = (*stack)->n;
