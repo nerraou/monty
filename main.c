9c35e90172be27fb69808923b0449ee0dcddc27a
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-27 16:01:12 +0100

    close fd

--- main.c
+++ main.c
@@ -5,9 +5,12 @@
  * print_and_exit - print error and exit
  * @message: error message
  * @arg: message argument
+ * @fd: file handle
  */
-void print_and_exit(char *message, char *arg)
+void print_and_exit(char *message, char *arg, int fd)
 {
+	if (fd >= 0)
+		close(fd);
 	if (arg)
 	{
 		fprintf(stderr, "%s %s\n", message, arg);
@@ -25,11 +28,13 @@ void print_and_exit(char *message, char *arg)
  * @trimed: trimed line
  * @stack: stack
  * @l_num: line number
+ * @fd: file handle
  */
-void protected_execute(char *trimed, stack_t **stack, int l_num)
+void protected_execute(char *trimed, stack_t **stack, int l_num, int fd)
 {
 	if (_execute(trimed, stack, l_num) == 0)
 	{
+		close(fd);
 		free(trimed);
 		free_dlistint(*stack);
 		exit(EXIT_FAILURE);
@@ -53,17 +58,17 @@ int main(int ac, char *av[])
 
 	stack = NULL;
 	if (ac != 2)
-		print_and_exit("USAGE: monty file", NULL);
+		print_and_exit("USAGE: monty file", NULL, -1);
 	else
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd < 0)
-			print_and_exit("Error: Can't open file", av[1]);
+			print_and_exit("Error: Can't open file", av[1], -1);
 		while (1)
 		{
 			size = _getline(fd, &line);
 			if (size == -1)
-				print_and_exit("Error: malloc failed", NULL);
+				print_and_exit("Error: malloc failed", NULL, fd);
 			if (size == 0)
 				break;
 			trimed = _strtrim(line, " \t\n\r");
@@ -71,14 +76,15 @@ int main(int ac, char *av[])
 			if (!trimed)
 			{
 				free_dlistint(stack);
-				print_and_exit("Error: malloc failed", NULL);
+				print_and_exit("Error: malloc failed", NULL, fd);
 			}
 			if (trimed[0] != '\0')
-				protected_execute(trimed, &stack, line_number);
+				protected_execute(trimed, &stack, line_number, fd);
 			free(trimed);
 			line_number++;
 		}
 		free_dlistint(stack);
+		close(fd);
 	}
-	return (0);
+	return (EXIT_SUCCESS);
 }
